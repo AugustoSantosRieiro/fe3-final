3a376c1fd4826f6466bfcc334767810460be3dc9
AugustoSantosRieiro <augustosantosrieiro@hotmail.com>
2023-09-15 12:47:58 -0300

    entregando correcciones 3.0

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,43 +1,44 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Card from '../Components/Card';
 
 const Detail = () => {
- const {id} = useParams()
-  const DentistFetch = (id) => {
+  const { id } = useParams();
+  const [dentistaByid, setDentistaById] = useState({});
+  
+  useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/users/${id}`;
-    const [resp, setResp] = useState([]);
-  
-    try {
-      const getFetch = async () => {
-        const { data } = await axios.get(url);
-        setResp(data);
-      };
-      useEffect(() => {
-        getFetch();
-      }, []);
-    } catch (error) {
-    }
-  
-    return resp;
-  };
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('La respuesta de la red no fue exitosa');
+        }
+        const data = await response.json();
+        setDentistaById(data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
 
- const dentistaByid= DentistFetch(id)
+    fetchData();
+  }, [id]);
+
   return (
     <>
-      <h1>Detalles del Dentista </h1>
-     <div style={{display:'flex',justifyContent:'center',color:'black'}}>
-     <Card 
-      name={dentistaByid.name}
-      username={dentistaByid.username}
-      email={dentistaByid.email}
-      phone={dentistaByid.phone}
-      website={dentistaByid.website}
-      />
-     </div>
+      <h1>Detalles del Dentista</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', color: 'black' }}>
+        <Card
+          name={dentistaByid.name}
+          username={dentistaByid.username}
+          email={dentistaByid.email}
+          phone={dentistaByid.phone}
+          website={dentistaByid.website}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
